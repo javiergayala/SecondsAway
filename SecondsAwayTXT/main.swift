f0e93095c1157a205c46164e9b23c4f0e5983fe9
Javier Ayala <jayala@rackspace.com>
2014-12-05 13:33:10 -0600

    Initial Commit

---
+++ SecondsAwayTXT/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SecondsAwayTXT
+//
+//  Created by Javier Ayala on 12/5/14.
+//  Copyright (c) 2014 Javier Ayala. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
